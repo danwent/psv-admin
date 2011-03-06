b0cf7153e23cd9d03bf09d408eba508e0ea11cf6
Dan Wendlandt <danwent@gmail.com>
2011-03-06 14:56:49 -0800

    start/stop/restart scripts and logging for webserver

--- stop_webserver.sh
+++ stop_webserver.sh
@@ -1,3 +1,14 @@
 #!/bin/bash 
 
-kill `ps -Af | grep notary_http.py | grep -v grep | awk '{print $2}'`
+pid=`ps -Af | grep notary_http.py | grep -v grep | awk '{print $2}'`
+
+if [ -n "$pid" ]
+then
+	kill $pid 
+	echo "Stopped notary_http.py"
+	date=`date`
+	echo "notary_http.py stopped from script at $date" >> logs/webserver.log
+else
+	echo "No notary_http.py was running"
+fi 
+
