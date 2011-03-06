56da3704217e9927f75d7ff1ee84a6a2d9f60499
Dan Wendlandt <danwent@gmail.com>
2011-03-06 02:30:00 -0800

    do not overwrite existing dirs/files

--- setup.sh
+++ setup.sh
@@ -6,14 +6,26 @@ then
 	exit 1
 fi 
 
-mkdir logs
+if ! [ -d logs ] 
+then 
+	mkdir logs
+fi 
 
 # setup DB file and notary keys 
 cd Perspectives-Server
-python utilities/create_tables.py notary.sqlite
-bash utilities/create_key_pair.sh notary.priv notary.pub
-grep "Start Host\|End Host" $1 > no_keys.txt
-python utilities/file2b2.py no_keys.txt notary.sqlite
+
+if ! [ -f notary.sqlite ]
+then
+	python utilities/create_tables.py notary.sqlite
+	grep "Start Host\|End Host" $1 > no_keys.txt
+	python utilities/file2b2.py no_keys.txt notary.sqlite
+fi 
+
+if ! [ -f notary.priv ] 
+then
+	bash utilities/create_key_pair.sh notary.priv notary.pub
+fi 
+
 cd ..
 
 # setup crontab
@@ -24,15 +36,19 @@ if ! [ -f ~/.ssh/id_rsa ]
 then 
 	echo "Generating new SSH key"
 	ssh-keygen -t rsa
+	echo "SSH public key:" 
+	cat ~/.ssh/id_rsa.pub
 fi 
  
-echo "SSH public key:" 
-cat ~/.ssh/id_rsa.pub
 
-mkdir notary_backup
-cd notary_backup 
-git init
-git remote add origin gitosis@www.networknotary.org:$2.git
-git config user.name "$2"
-git config user.email "<none>"
-git pull origin master # in case this is a rebuild
+if ! [ -d notary_backup ]  
+then 
+	mkdir notary_backup
+	cd notary_backup 
+	git init
+	git remote add origin gitosis@www.networknotary.org:$2.git
+	git config user.name "$2"
+	git config user.email "<none>"
+	git pull origin master # in case this is a rebuild
+fi 
+
