db06c58881e27f57c52547ab8c6656cf01b93a8c
Dan Wendlandt <danwent@gmail.com>
2011-03-07 18:22:16 -0800

    improve stop scripts

--- stop_scan.sh
+++ stop_scan.sh
@@ -1,5 +1,12 @@
 #!/bin/bash 
 
-date=`date`
-echo "killing scanner from script at $date" >> logs/scanner.log
-kill `ps -Af | grep threaded_scanner.py | grep -v grep | awk '{print $2}'`
+pid=`ps -Af | grep "python threaded_scanner.py" | grep -v grep | awk '{print $2}'`
+if [ -n "$pid" ]; 
+then
+	date=`date`
+	echo "killing scanner from script at $date" >> logs/scanner.log
+	kill $pid
+else
+	echo "Scanner is not running"
+fi
+
