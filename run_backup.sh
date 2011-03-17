947ff2b09d642ba8f77dccf408378fa4b9732c96
Dan Wendlandt <danwent@gmail.com>
2011-03-17 13:37:14 -0700

    for backup, remove bz2 file before generating a new one

--- run_backup.sh
+++ run_backup.sh
@@ -3,6 +3,7 @@
 cd Perspectives-Server
 python utilities/db2file.py notary.sqlite ../notary_backup/notary_dump.txt
 cd ../notary_backup
+rm notary_dump.txt.bz2
 bzip2 notary_dump.txt
 git add notary_dump.txt.bz2
 DATE=`date`
